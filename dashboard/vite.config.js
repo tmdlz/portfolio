b8dd1191eedd40b83c7ff5d22e61ffbe3a107ccc
tmdlz <parisdistro@Pariss-MacBook-Air.local>
2026-02-08 03:03:17 +0100

    add dashboard build files

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',  // ← chemin relatif, marche partout
+  build: {
+    outDir: '../dashboard-build/',
+  }
+
+
 })
